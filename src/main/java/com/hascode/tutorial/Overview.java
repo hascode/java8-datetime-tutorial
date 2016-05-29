7631a69ef0ad72fcfabe99da9bfff3be87b4f9e3
Micha Kops <micha.kops@gmail.com>
2016-05-29 22:07:01 +0200

    Initial import.

---
+++ src/main/java/com/hascode/tutorial/Overview.java
@@ -0,0 +1,31 @@
+package com.hascode.tutorial;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZonedDateTime;
+
+public class Overview {
+
+	public static void main(String[] args) {
+
+		// just a date, to time, no time-zone, example: my birthday this year
+		LocalDate ld = LocalDate.now();
+		System.out.println("LocalDate\t:" + ld);
+
+		// just a time, no date, no time-zone, example: midnight
+		LocalTime lt = LocalTime.now();
+		System.out.println("LocalTime\t:" + lt);
+
+		// date and time without a time-zone, example: midnight on new year's
+		// eve
+		LocalDateTime ldt = LocalDateTime.now();
+		System.out.println("LocalDateTime\t:" + ldt);
+
+		// date, time and time-zone specified, example: meeting at 5:00 p.m.
+		// CET"
+		ZonedDateTime zdt = ZonedDateTime.now();
+		System.out.println("ZonedDateTime\t:" + zdt);
+	}
+
+}
